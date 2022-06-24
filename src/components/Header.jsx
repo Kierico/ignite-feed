ffab9ca0bc00db9d88ef185a63097c253205dc7f
Kiérico Souza <kiericograndao@gmail.com>
2022-06-24 15:07:32 -0400

    2.3 - Componente: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import styles from "./Header.module.css";
 
+import kiericoImg from "../assets/images/icon1-kierico.png";
+
 export function Header() {
     return (
         <header className={styles.header}>
+            <img src={kiericoImg} alt="Logo Kiérico" />
             <strong>Kiérico Feed</strong>
         </header>
     );
