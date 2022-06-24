9b96efaf75616710ee53b7b70a87e8a0ba946bcc
Kiérico Souza <kiericograndao@gmail.com>
2022-06-24 15:48:31 -0400

    2.4 - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import React from "react"
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
 export function App() {
 
   return (
     <React.Fragment>
       <Header />
-      <Post
-        author="Kiérico"
-        content="Olá, tenho 37 anos, sou estudante do curso de Ciência da Computação."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Kiérico"
+            content="Olá, tenho 37 anos, sou estudante do curso de Ciência da Computação."
+          />
+        </main>
+      </div>
     </React.Fragment>
   )
 }
