298554b893c1ad3008e2639b4b45a14b10a5a3c1
Kiérico Souza <kiericograndao@gmail.com>
2022-06-25 11:34:16 -0400

    2.9 - Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,30 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/kierico.png" alt="" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Kiérico Souza</strong>
+                            <time title="24 de Junho de 2022 às 18:27h" dateTime="2022-06-24 18:27:00">Cerca de 1h atrás</time>
+                        </div>
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom Kiérico, parabéns!! 👏👏</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp size={20} />
+                        Apludir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
