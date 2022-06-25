11106d1d42505efd9393f78f17dee7154a90b02e
Kiérico Souza <kiericograndao@gmail.com>
2022-06-25 11:11:13 -0400

    2.8 - Formulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -22,6 +22,16 @@ export function Post() {
                     <a href="#">#novoprojeto</a>{" "}
                     <a href="#">#kierico</a></p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    placeholder="Deixe um comentário"
+                />
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
         </article>
     );
 }
