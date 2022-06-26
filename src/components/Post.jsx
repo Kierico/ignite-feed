51e8943e3c3dcabbd2dbe69f316bb2cc8cc786ab
Kiérico Souza <kiericograndao@gmail.com>
2022-06-26 19:44:50 -0400

    3.8 - Validando formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,7 +30,12 @@ export function Post({ author, content, publishedAt }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity("");
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity("Esse campo é obrigatório!");
     }
 
     function deleteComment(commentToDelete) {
@@ -39,6 +44,8 @@ export function Post({ author, content, publishedAt }) {
         })
         setComments(commentsWithoutDeleteOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={styles.post}>
@@ -72,9 +79,13 @@ export function Post({ author, content, publishedAt }) {
                     placeholder="Deixe um comentário"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    required
+                    onInvalid={handleNewCommentInvalid}
                 />
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
